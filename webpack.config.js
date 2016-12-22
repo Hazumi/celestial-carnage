07c372ddeb6228f65b4d6600cfb80a7a672ea34d
Hazumi <shirohazumi@openmailbox.org>
2016-12-22 16:40:37 -0500

    inital commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./scripts/main.js",
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  output: {
+    path: __dirname + "/scripts",
+    filename: "main.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
