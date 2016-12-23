83232128265f4934738bd2f019eac43e77627cd9
Hazumi <shirohazumi@openmailbox.org>
2016-12-22 21:06:10 -0500

    added planet

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,21 @@ const $ = require('jquery');
 
 $(document).ready(function() {
 
-  game = {
-    player: {
-      health: 10,
-      maxHealth: 10
-    },
-    gameLoop: {
-
-    }
+  const player = {
+    hp: 10,
+    maxHp: 10,
+    sta: 20,
+    maxSta: 20,
+    exp: 0,
+    maxExp: 100
   }
+
+  function showStats() {
+
+  }
+
+  window.setInterval(function() {
+    console.log('hi');
+  }, 1000);
 
 });
