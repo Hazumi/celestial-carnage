45ed25fd1b829974e6ba22000f984f7a889137cc
Hazumi <shirohazumi@openmailbox.org>
2016-12-23 19:56:34 -0500

    started combat

--- scripts/main.js
+++ scripts/main.js
@@ -10,27 +10,42 @@ $(document).ready(function() {
     },
     cacheDom: function() {
       this.$explore = $('#btn-explore');
+      this.$playerHp = $('#playerHp');
+      this.$playerMaxHp = $('#playerMaxHp');
+      this.$playerSta = $('#playerSta');
+      this.$playerMaxSta = $('#playerMaxSta');
+      this.$playerExp = $('#playerExp');
+      this.$playerMaxExp = $('#playerMaxExp');
     },
     bindEvents: function() {
       this.$explore.on('click', function() {
-        // spawn phanton
+        game.combat(phantom);
       });
     },
     gameLoop: function() {
-      console.log('test');
+      window.setInterval(function() {
+
+        // if (combat) {
+          if (game.$playerHp.val() !== player.hp) game.$playerHp.html(player.hp);
+          if (game.$playerSta.val() !== player.sta) game.$playerSta.html(player.sta);
+        // }
+
+      }, 1000)
     },
+    combat: function(enemy) {
+      let currentEnemy = phantom;
+      console.log(currentEnemy);
+    }
+
   }
 
-  window.setInterval(function() {
-    // displayStats();
-  }, 1000);
-
   let player = {
+    name: "",
     rank: 1,
     hp: 10,
     maxHp: 10,
-    sta: 20,
-    maxSta: 20,
+    sta: 10,
+    maxSta: 10,
     exp: 0,
     maxExp: 100
   }
@@ -38,9 +53,11 @@ $(document).ready(function() {
   let phantom = {
     rank: 1,
     hp: 10,
-    sta: 20,
+    sta: 10,
     exp: 2
   }
+
+
 
   game.init();
 
