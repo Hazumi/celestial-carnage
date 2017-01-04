2dd5dd214b73cfbb433d2543eea4a5b852a27aa3
Hazumi <shirohazumi@openmailbox.org>
2017-01-04 06:11:46 -0500

    new layout

--- scripts/main.js
+++ scripts/main.js
@@ -16,6 +16,8 @@ $(document).ready(function() {
       this.$playerMaxSta = $('#playerMaxSta');
       this.$playerExp = $('#playerExp');
       this.$playerMaxExp = $('#playerMaxExp');
+      this.$laser = $('#laser');
+      this.$log = $('#log');
     },
     bindEvents: function() {
       this.$explore.on('click', function() {
@@ -25,16 +27,39 @@ $(document).ready(function() {
     gameLoop: function() {
       window.setInterval(function() {
 
+
         // if (combat) {
-          if (game.$playerHp.val() !== player.hp) game.$playerHp.html(player.hp);
-          if (game.$playerSta.val() !== player.sta) game.$playerSta.html(player.sta);
+          function updateStats() {
+            if (game.$playerHp.val() !== player.hp) game.$playerHp.html(player.hp);
+            if (game.$playerSta.val() !== player.sta) game.$playerSta.html(player.sta);
+          }
+          console.log('check');
+
         // }
+
+        // check for 0 hp
 
       }, 1000)
     },
     combat: function(enemy) {
       let currentEnemy = phantom;
-      console.log(currentEnemy);
+
+      this.$laser.on('click', function() {
+        currentEnemy.hp -= player.atk;
+        var dmgGiven = currentEnemy.hp -= player.atk;
+        game.$log.append( 'You hit ' +  currentEnemy.name + ' for ' + dmgGiven  + ' damage.' + '<br>');
+
+        var dmgTaken = player.hp -= currentEnemy.atk;
+        player.hp -= currentEnemy.atk;
+      });
+
+
+      // function checkFirst(currentEnemy) {
+      //   if (player.rank > currentEnemy.rank) {
+      //
+      //   }
+      // }
+
     }
 
   }
@@ -46,11 +71,13 @@ $(document).ready(function() {
     maxHp: 10,
     sta: 10,
     maxSta: 10,
+    atk: 1,
     exp: 0,
     maxExp: 100
   }
 
   let phantom = {
+    name: 'Phantom',
     rank: 1,
     hp: 10,
     sta: 10,
