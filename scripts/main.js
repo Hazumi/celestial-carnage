c7dd7b0e7908b033e24dc1531fb90b04ebd5aaa5
Hazumi <shirohazumi@openmailbox.org>
2017-02-19 01:42:31 -0500

    added travel

--- scripts/main.js
+++ scripts/main.js
@@ -1,91 +1,44 @@
-const $ = require('jquery');
+const stationBtn = document.getElementById('stationBtn');
+const station = document.getElementById('station');
+const researchLabBtn = document.getElementById('researchLabBtn');
+const researchLab = document.getElementById('research-lab');
+const playerPwr = document.getElementById('playerPwr');
+const playerMaxPwr = document.getElementById('playerMaxPwr');
 
-$(document).ready(function() {
+let player = {
+ pwr: 100,
+ maxPwr: 100,
+ location: 'station'
+};
 
-  const game = {
-    init: function() {
-      this.cacheDom();
-      this.bindEvents();
-      this.gameLoop();
-    },
-    cacheDom: function() {
-      this.$explore = $('#btn-explore');
-      this.$playerHp = $('#playerHp');
-      this.$playerMaxHp = $('#playerMaxHp');
-      this.$playerSta = $('#playerSta');
-      this.$playerMaxSta = $('#playerMaxSta');
-      this.$playerExp = $('#playerExp');
-      this.$playerMaxExp = $('#playerMaxExp');
-      this.$laser = $('#laser');
-      this.$log = $('#log');
-    },
-    bindEvents: function() {
-      this.$explore.on('click', function() {
-        game.combat(phantom);
-      });
-    },
-    gameLoop: function() {
-      window.setInterval(function() {
+function allHidden() {
+  station.classList.add('hidden');
+  researchLab.classList.add('hidden');
+}
 
-
-        // if (combat) {
-          function updateStats() {
-            if (game.$playerHp.val() !== player.hp) game.$playerHp.html(player.hp);
-            if (game.$playerSta.val() !== player.sta) game.$playerSta.html(player.sta);
-          }
-          console.log('check');
-
-        // }
-
-        // check for 0 hp
-
-      }, 1000)
-    },
-    combat: function(enemy) {
-      let currentEnemy = phantom;
-
-      this.$laser.on('click', function() {
-        currentEnemy.hp -= player.atk;
-        var dmgGiven = currentEnemy.hp -= player.atk;
-        game.$log.append( 'You hit ' +  currentEnemy.name + ' for ' + dmgGiven  + ' damage.' + '<br>');
-
-        var dmgTaken = player.hp -= currentEnemy.atk;
-        player.hp -= currentEnemy.atk;
-      });
-
-
-      // function checkFirst(currentEnemy) {
-      //   if (player.rank > currentEnemy.rank) {
-      //
-      //   }
-      // }
-
-    }
-
+function travel(destination) {
+  if (player.location != destination && player.pwr >= 5) {
+    player.location = destination;
+    player.pwr -= 5;
+    allHidden();
+  if (playerPwr.innerHTML != player.pwr) playerPwr.innerHTML = player.pwr;
   }
+}
 
-  let player = {
-    name: "",
-    rank: 1,
-    hp: 10,
-    maxHp: 10,
-    sta: 10,
-    maxSta: 10,
-    atk: 1,
-    exp: 0,
-    maxExp: 100
-  }
-
-  let phantom = {
-    name: 'Phantom',
-    rank: 1,
-    hp: 10,
-    sta: 10,
-    exp: 2
-  }
-
-
-
-  game.init();
-
+stationBtn.addEventListener('click', function() {
+  travel('station');
+  station.classList.remove('hidden');
 });
+
+researchLabBtn.addEventListener('click', function() {
+  travel('researchLab');
+  researchLab.classList.remove('hidden');
+});
+
+
+window.setInterval(function() {
+  if (playerPwr.innerHTML != player.pwr) playerPwr.innerHTML = player.pwr;
+  if (player.location == 'station' && player.pwr < player.maxPwr) {
+    player.pwr += 1;
+  }
+}, 1000);
